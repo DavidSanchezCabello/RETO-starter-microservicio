46106afab0b8171f775e1acf7abe025d4cb71283
David <david.cabello.info@gmail.com>
2021-12-01 17:15:12 +0100

    update

--- src/main/java/com/dscode/aplication/controller/IndexController.java
+++ src/main/java/com/dscode/aplication/controller/IndexController.java
@@ -3,14 +3,15 @@ package com.dscode.aplication.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import starter.Convertir;
 
 
@@ -20,27 +21,27 @@ public class IndexController {
 	@Autowired
 	private Convertir convertir;
 	
-	private Counter contadorCel;
 	private Counter contadorFar;
 	
 	private final static Logger logger = LoggerFactory.getLogger(IndexController.class);
 	
-	public IndexController() {
-		
+	public IndexController(MeterRegistry meterRegistry) {
+		this.contadorFar=Counter.builder("Invocaciones mas usadas").description("Total de invocaciones para Grados").register(meterRegistry);
 	}
 	
 	
-	@GetMapping("/celAfar")
-	public ResponseEntity<String> index() {
+	@GetMapping("/conversor/{status}")
+	public ResponseEntity<String> index(@PathVariable("status") String status) {
 		contadorFar.increment();
 		logger.info("Realizamos una operación " + contadorFar.count());
-		return new ResponseEntity<String>(HttpStatus.OK).ok(convertir.mensajeConversion());
+		return new ResponseEntity<String>(HttpStatus.OK).ok(convertir.mensajeConversion(status));
 	}
 	
-	@GetMapping("/farAcel")
-	public ResponseEntity<String> saludo(@RequestParam("status") String status) {
-		contadorCel.increment();
-		logger.info("Realizamos una operación " + contadorCel.count());
-		return new ResponseEntity<String>(HttpStatus.OK).ok("Todo ha salido Okey");
+	@GetMapping("/conversor1/{status}")
+	public String prueba(@PathVariable("status") String status) {
+		contadorFar.increment();
+		logger.info("Realizamos una operación " + contadorFar.count());
+		return status;
 	}
+	
 }
